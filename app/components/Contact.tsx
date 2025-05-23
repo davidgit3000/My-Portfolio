a943a305c02726f6d0d8295506bfa7559a759905
David Lam <davidkarik5@gmail.com>
2025-05-22 22:17:27 -0700

    fixed the ESLint error in Contact page

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -2,7 +2,7 @@
 
 import { useRef, useState } from "react";
 import { motion, useInView } from "framer-motion";
-import { EnvelopeIcon, GlobeAltIcon } from "@heroicons/react/24/outline";
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
 import { ArrowPathIcon } from "@heroicons/react/24/solid";
 import { SiGithub, SiLinkedin } from "react-icons/si";
 
